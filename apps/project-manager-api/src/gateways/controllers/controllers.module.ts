9cd58e2687707b48033c26cb103fabcb0ade073a
gabriel1og <gogomide.go@gmail.com>
2025-09-19 12:13:53 -0300

    feat: add redis transport

--- apps/project-manager-api/src/gateways/controllers/controllers.module.ts
+++ apps/project-manager-api/src/gateways/controllers/controllers.module.ts
@@ -5,9 +5,16 @@ import { UsersController } from './users/users.controller';
 import { UseCasesModule } from '@project-manager-api/domain/use-cases/use-cases.module';
 import { AuthController } from './auth/auth.controller';
 import { AuthModule } from '@project-manager-api/infrastructure/auth/auth.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [UseCasesModule, AuthModule],
+  imports: [
+    UseCasesModule,
+    AuthModule,
+    ClientsModule.register([
+      { name: 'PROJECTS_MANAGER_API', transport: Transport.REDIS },
+    ]),
+  ],
   controllers: [
     ProjectsController,
     TasksController,
--- apps/tasks/src/main.ts
+++ apps/tasks/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { TasksModule } from './tasks.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(TasksModule);
-  await app.listen(process.env.port ?? 3000);
+  app.connectMicroservice(
+    {
+      transport: Transport.REDIS,
+      options: {
+        host: 'localhost',
+        port: 6379,
+      },
+    },
+    {
+      inheritAppConfig: true,
+    },
+  );
+  await app.startAllMicroservices();
 }
 bootstrap();
