eda5c5ec66009b2e990018e660e2396b9403d268
gabriel1og <gogomide.go@gmail.com>
2025-09-19 02:37:12 -0300

    feat: create gateways and add guard globally

--- src/gateways/controllers/controllers.module.ts
+++ src/gateways/controllers/controllers.module.ts
@@ -3,9 +3,10 @@ import { ProjectsController } from './projects/projects.controller';
 import { TasksController } from './tasks/tasks.controller';
 import { UsersController } from './users/users.controller';
 import { UseCasesModule } from 'src/domain/use-cases/use-cases.module';
+import { AuthController } from './auth/auth.controller';
 
 @Module({
   imports: [UseCasesModule],
-  controllers: [ProjectsController, TasksController, UsersController],
+  controllers: [ProjectsController, TasksController, UsersController, AuthController],
 })
 export class ControllersModule {}
