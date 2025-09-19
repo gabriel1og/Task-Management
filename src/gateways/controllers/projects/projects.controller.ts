a9cbcf65c5ba429e7112de32afb3f45342acd6da
gabriel1og <gogomide.go@gmail.com>
2025-09-19 02:52:02 -0300

    feat: add cache

--- src/gateways/controllers/projects/projects.controller.ts
+++ src/gateways/controllers/projects/projects.controller.ts
@@ -13,6 +13,7 @@ import { CreateProjectService } from 'src/domain/use-cases/projects/create-proje
 import { GetAllProjectsService } from 'src/domain/use-cases/projects/get-all-projects.service';
 import { GetProjectByIdService } from 'src/domain/use-cases/projects/get-project-by-id.service';
 import { CreateProjectDto } from './dtos/create-project.dto';
+import { CACHE_MANAGER, Cache } from '@nestjs/cache-manager';
 
 @Controller('projects')
 export class ProjectsController {
@@ -20,12 +21,23 @@ export class ProjectsController {
     private readonly getAllProjectsUseCase: GetAllProjectsService,
     private readonly getProjectByIdUseCase: GetProjectByIdService,
     private readonly createProjectUseCase: CreateProjectService,
+    @Inject(CACHE_MANAGER) private cacheService: Cache,
   ) {}
   @Get()
   async findAll(@Req() request) {
     try {
       const loggedUser = request.user;
-      return await this.getAllProjectsUseCase.execute(loggedUser.sub);
+      const cachedData = await this.cacheService.get<{ name: string }>(
+        `user-${loggedUser.sub}/all-projects`,
+      );
+      console.log(cachedData);
+      if (cachedData) {
+        console.log(`Getting data from cache!`);
+        return cachedData;
+      }
+      const data = await this.getAllProjectsUseCase.execute(loggedUser.sub);
+      await this.cacheService.set(`user-${loggedUser.sub}/all-projects`, data);
+      return data;
     } catch (error) {
       throw new NotFoundException(error.message);
     }
--- src/infrastructure/redis/redis.module.ts
+++ src/infrastructure/redis/redis.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { CacheModule } from '@nestjs/cache-manager';
 
-@Module({})
+@Module({
+  imports: [
+    CacheModule.register({
+      isGlobal: true,
+      ttl: 10000,
+    }),
+  ],
+})
 export class RedisModule {}
