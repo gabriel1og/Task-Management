35df76df54c1d85fe87e5c67f7015bee628230c6
gabriel1og <gogomide.go@gmail.com>
2025-09-18 22:30:19 -0300

    first commit

---
+++ src/domain/entities/user.ts
@@ -0,0 +1,30 @@
+import { IProject } from '../interfaces/project.interface';
+import { ITask } from '../interfaces/task.interface';
+import { IUser } from '../interfaces/user.interface';
+
+export class User implements IUser {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  projects: IProject[];
+  tasks: ITask[];
+  constructor(
+    id: number,
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string,
+    projects: [],
+    tasks: [],
+  ) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.password = password;
+    this.projects = projects;
+    this.tasks = tasks;
+  }
+}
