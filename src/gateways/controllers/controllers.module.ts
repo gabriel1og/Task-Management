1b6a0a3d1a3d856154020cf469ab5713de66ec4f
gabriel1og <gogomide.go@gmail.com>
2025-09-19 02:46:47 -0300

    feat: add auth on controllers

--- src/gateways/controllers/controllers.module.ts
+++ src/gateways/controllers/controllers.module.ts
@@ -4,9 +4,15 @@ import { TasksController } from './tasks/tasks.controller';
 import { UsersController } from './users/users.controller';
 import { UseCasesModule } from 'src/domain/use-cases/use-cases.module';
 import { AuthController } from './auth/auth.controller';
+import { AuthModule } from 'src/infrastructure/auth/auth.module';
 
 @Module({
-  imports: [UseCasesModule],
-  controllers: [ProjectsController, TasksController, UsersController, AuthController],
+  imports: [UseCasesModule, AuthModule],
+  controllers: [
+    ProjectsController,
+    TasksController,
+    UsersController,
+    AuthController,
+  ],
 })
 export class ControllersModule {}
--- src/gateways/controllers/projects/projects.controller.ts
+++ src/gateways/controllers/projects/projects.controller.ts
@@ -2,9 +2,11 @@ import {
   Body,
   Controller,
   Get,
+  Inject,
   NotFoundException,
   Param,
   Post,
+  Req,
   UnprocessableEntityException,
 } from '@nestjs/common';
 import { CreateProjectService } from 'src/domain/use-cases/projects/create-project.service';
@@ -12,7 +14,6 @@ import { GetAllProjectsService } from 'src/domain/use-cases/projects/get-all-pro
 import { GetProjectByIdService } from 'src/domain/use-cases/projects/get-project-by-id.service';
 import { CreateProjectDto } from './dtos/create-project.dto';
 
-const userId = 1;
 @Controller('projects')
 export class ProjectsController {
   constructor(
@@ -21,18 +22,20 @@ export class ProjectsController {
     private readonly createProjectUseCase: CreateProjectService,
   ) {}
   @Get()
-  async findAll() {
+  async findAll(@Req() request) {
     try {
-      return await this.getAllProjectsUseCase.execute(userId);
+      const loggedUser = request.user;
+      return await this.getAllProjectsUseCase.execute(loggedUser.sub);
     } catch (error) {
       throw new NotFoundException(error.message);
     }
   }
   @Get(':id')
-  async findOne(@Param('id') id: number) {
+  async findOne(@Req() request, @Param('id') id: number) {
     try {
+      const loggedUser = request.user;
       return await this.getProjectByIdUseCase.execute({
-        userId,
+        userId: loggedUser.sub,
         projectId: id,
       });
     } catch (error) {
@@ -40,10 +43,11 @@ export class ProjectsController {
     }
   }
   @Post()
-  async create(@Body() createProjectDto: CreateProjectDto) {
+  async create(@Req() request, @Body() createProjectDto: CreateProjectDto) {
     try {
+      const loggedUser = request.user;
       return await this.createProjectUseCase.execute({
-        userId,
+        userId: loggedUser.sub,
         project: createProjectDto,
       });
     } catch (error) {
